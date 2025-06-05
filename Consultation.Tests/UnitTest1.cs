7d74d4f4f36ede34fd92eca46ece067e43993415
reygiansan <reygiansan61@gmail.com>
2025-06-05 15:16:55 +0800

    Delete Class Library Folder and Change Project folder to NUnit Project Folder

---
+++ Consultation.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Consultation.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
